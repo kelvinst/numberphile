346b29424ba0c8d2a2103e1ff7bf098a874459c2
kelvinst <kelvin.stinghen@gmail.com>
2018-09-04 17:30:34 -0300

    Initial commit

---
+++ lib/tcsr.ex
@@ -0,0 +1,18 @@
+defmodule TCSR do
+  @moduledoc """
+  Documentation for TCSR.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TCSR.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tcsr_test.exs
@@ -0,0 +1,8 @@
+defmodule TCSRTest do
+  use ExUnit.Case
+  doctest TCSR
+
+  test "greets the world" do
+    assert TCSR.hello() == :world
+  end
+end
